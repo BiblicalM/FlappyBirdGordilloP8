75ce3e7c419d851e0edc7583736b24474365a932
Maximiliano Gordillo <724910@student.dusd.net>
2023-12-09 01:05:15 -0800

    penguin animations
    added the penguin, ground, day background. gave the penguin and ground collision, and then added animations to the penguin.

---
+++ FlappyBirdGordilloP8/Assets/Scripts/Penguin.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Penguin : MonoBehaviour
+{
+
+    public float upForce = 200f;
+
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+    private Animator anim;
+    
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        if (!isDead)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+                anim.SetTrigger("Flap");
+            }
+        }
+    }
+
+    void OnCollisionEnter2D()
+    {
+        isDead = true;
+        anim.SetTrigger("Die");
+    }
+}
